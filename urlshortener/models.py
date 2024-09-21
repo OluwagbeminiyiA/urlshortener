9ae9d13dfd17ce564f3c00b6c99fcf7aec28e67c
Oluwagbeminiyi Agbedejobi <gagbedejobi@gmail.com>
2024-09-21 09:58:22 +0100

    altered the url fields in models.py to accept only valid urls

--- urlshortener/models.py
+++ urlshortener/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class URL(models.Model):
     key = models.CharField(max_length=155, default='xxxxx')
-    url = models.CharField(max_length=1000)
-    short_url = models.CharField(max_length=1000)
+    url = models.URLField(max_length=1000)
+    short_url = models.URLField(max_length=1000)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
